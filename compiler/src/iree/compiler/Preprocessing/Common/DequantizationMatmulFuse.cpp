84c5a60ea2f0819217d4773641d90e335f068a80
Max Dawkins <max.dawkins@gmail.com>
2023-07-17 11:53:05 -0400

    pass for fusing dequantization and matmul linalg generic ops working

--- compiler/src/iree/compiler/Preprocessing/Common/DequantizationMatmulFuse.cpp
+++ compiler/src/iree/compiler/Preprocessing/Common/DequantizationMatmulFuse.cpp
@@ -12,6 +12,7 @@
 #include "mlir/Pass/Pass.h"
 #include "mlir/Support/LLVM.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include <iostream>
 
 namespace mlir {
 namespace iree_compiler{
@@ -19,16 +20,80 @@ namespace IREE{
 
 namespace {
 
+
+void fuseDequantAndMatmul(RewriterBase &rewriter,
+                          Operation* dequant,
+                          Operation* matmul){
+  Value mmResultValue = matmul->getResult(0);
+  auto mmResultType =
+      llvm::cast<RankedTensorType>(matmul->getResult(0).getType());
+
+  SmallVector<Value> workload;
+  // Flow::DispatchRegionOp Flow::makeEmptyDispatchRegion(rewriter, genOpMatmul.getLoc(), workload)
+  rewriter.setInsertionPointAfter(matmul);
+
+  OpBuilder::InsertionGuard guard(rewriter);
+
+  // Create RegionOp.
+  auto regionOp = rewriter.create<Flow::DispatchRegionOp>(
+      matmul->getLoc(), /*resultTypes=*/mmResultType, /*dynamicDims=*/ValueRange(), workload);
+  Block &body = regionOp.getBody().emplaceBlock();
+  rewriter.setInsertionPointToStart(&body);
+  rewriter.create<Flow::ReturnOp>(matmul->getLoc(), mmResultValue);
+  
+  // Values replaced by moving the `targets` into the dispatch region.
+  Value replacedOutputValue;
+
+  // New value that is yielded from dispatch.
+  Value yieldedResult;
+  
+  Value regionOpResult = regionOp->getResult(0);
+  // std::cout << "regionOp result: " << &regionOpResult << std::endl;
+  // std::cout << "matmul result: " << &mmResultValue << std::endl;
+  rewriter.replaceAllUsesWith(mmResultValue, regionOpResult);
+  // std::cout << "regionOp use num: " << regionOp->hasOneUse() << std::endl;
+  // std::cout << "matmul use num: " << matmul->hasOneUse() << std::endl;
+
+  Block &body2 = regionOp.getBody().front();
+  rewriter.setInsertionPointToStart(&body2);
+  Operation *clonedMatmul = rewriter.clone(*matmul);
+  // replacedOutputValue = matmul->getResult(0);
+  yieldedResult = clonedMatmul->getResult(0);
+
+  // rewriter.replaceAllUsesWith(replacedOutputValue, yieldedResult);
+
+  auto regionReturnOp =
+      cast<Flow::ReturnOp>(regionOp.getBody().front().getTerminator());
+  rewriter.setInsertionPoint(regionReturnOp);
+  rewriter.replaceOpWithNewOp<Flow::ReturnOp>(regionReturnOp,
+                                              yieldedResult);
+
+  // auto returnOp = regionOp.getBody().front().getTerminator();
+  // rewriter.replaceOp(returnOp, yieldedResult);
+
+  Block &body3 = regionOp.getBody().front();
+  rewriter.setInsertionPointToStart(&body3);
+  Operation *clonedDequant = rewriter.clone(*dequant);
+  replacedOutputValue = dequant->getResult(0);
+  yieldedResult = clonedDequant->getResult(0);
+
+  rewriter.replaceAllUsesWith(replacedOutputValue, yieldedResult);
+
+  rewriter.eraseOp(matmul);
+  rewriter.eraseOp(dequant);
+
+  // std::cout << "mm parent: " << clonedMatmul->getParentOp() << std::endl;
+  // std::cout << "dq parent: " << clonedDequant->getParentOp() << std::endl;
+  // std::cout << "mm isAncestor: " << regionOp->isAncestor(clonedMatmul) << std::endl;
+  // std::cout << "dq isAncestor: " << regionOp->isAncestor(clonedDequant) << std::endl;
+
+  return;
+}
+
+
 //-----------------------------------------------------------//
 //                        Patterns
 //-----------------------------------------------------------//
-
-// struct DequantizationMatmulFusePass
-//     : public DequantizationMatmulFuseBase<DequantizationMatmulFusePass> {
-
-//   void getDependentDialects(DialectRegistry &registry) const override {
-//     registry.insert<linalg::LinalgDialect>();
-//   }
 
 class DequantizationMatmulFusePattern final
     : public OpRewritePattern<linalg::GenericOp> {
@@ -38,10 +103,20 @@ public:
   LogicalResult matchAndRewrite(linalg::GenericOp genOpMatmul,
                                 PatternRewriter &rewriter) const override {        
 
+    // Stop after op has been placed in a dispatch
+    // std::cout << "Starting..." << std::endl;
+
     // Match first generic op as matmul
 
     if (genOpMatmul.getNumResults() != 1) return failure();
-    if (genOpMatmul.getNumOperands() != 2) return failure();
+    if (genOpMatmul.getNumOperands() != 3) return failure();
+
+    Value mmRes = genOpMatmul->getResult(0);
+    auto matmulOp = mmRes.getDefiningOp();
+
+    // std::cout << "checking parent: " << matmulOp->getParentOp() << std::endl;
+    if (matmulOp->getParentOfType<Flow::DispatchRegionOp>()) return failure();
+    // std::cout << "no parent: " << matmulOp->getParentOp() << std::endl;
 
     // auto mmIndexingMaps = genOpMatmul.getIndexingMaps();
 
@@ -69,14 +144,15 @@ public:
     }
 
     Value mmLhs = genOpMatmul->getOperand(1);
-    auto mmLhsDefiningOp = mmLhs.getDefiningOp();
-    linalg::GenericOp genOpDequant = dyn_cast<linalg::GenericOp>(mmLhsDefiningOp);
+    auto dequantOp = mmLhs.getDefiningOp();
+
+    linalg::GenericOp genOpDequant = dyn_cast<linalg::GenericOp>(dequantOp);
     if (!genOpDequant) return failure();
 
     // Match defining op as dequantization
 
     if (genOpDequant.getNumResults() != 1) return failure();
-    if (genOpDequant.getNumOperands() != 3) return failure();
+    if (genOpDequant.getNumOperands() != 4) return failure();
 
     SmallVector<utils::IteratorType> dqIteratorTypes =
         genOpDequant.getIteratorTypesArray();
@@ -94,16 +170,74 @@ public:
 
     // Form dispatch region with dequantization and matmul
 
-    Flow::DispatchRegionOp regionOp;
-    auto maybeRegionOp = Flow::wrapOpInDispatchRegion(rewriter, genOpMatmul);
-    if (failed(maybeRegionOp))
-      return failure();
-    regionOp = *maybeRegionOp;
+    fuseDequantAndMatmul(rewriter, dequantOp, matmulOp);
+    // std::cout << "regionOp: " << regionOp << std::endl;
 
-    auto combinedRegionOp = Flow::movePrecedingOpsIntoDispatchRegion(rewriter, mmLhsDefiningOp, regionOp);
-    if (failed(combinedRegionOp))
-      return failure();
+    // Value mmResultValue = genOpMatmul.getResult(0);
+    // SmallVector<Value> workload;
+    // // Flow::DispatchRegionOp Flow::makeEmptyDispatchRegion(rewriter, genOpMatmul.getLoc(), workload)
+    // rewriter.setInsertionPointAfter(genOpMatmul);
+
+    // OpBuilder::InsertionGuard guard(rewriter);
+
+    // // Create RegionOp.
+    // auto regionOp = rewriter.create<Flow::DispatchRegionOp>(
+    //     genOpMatmul.getLoc(), /*resultTypes=*/mmResultType, /*dynamicDims=*/ValueRange(), workload);
+    // Block &body = regionOp.getBody().emplaceBlock();
+    // rewriter.setInsertionPointToStart(&body);
+    // rewriter.create<Flow::ReturnOp>(genOpMatmul.getLoc(), mmResultValue);
     
+    // // Values replaced by moving the `targets` into the dispatch region.
+    // // Value replacedOutputValue;
+
+    // // New value that is yielded from dispatch.
+    // // Value yieldedResult;
+
+    // // Operation *clonedTarget
+    // // Block &body
+    
+    // Block &body2 = regionOp.getBody().front();
+    // rewriter.setInsertionPointToStart(&body2);
+    // Operation *clonedMatmul = rewriter.clone(genOpMatmul);
+    // // replacedOutputValue = genOpMatmul.getResult(0);
+    // // yieldedResult = clonedTarget->getResult(0);
+    // // auto returnOp =
+    // //       cast<Flow::ReturnOp>(regionOp.getBody().front().getTerminator());
+    // // rewriter.replaceOp(clonedTarget, mmResultValue);
+
+    // Block &body3 = regionOp.getBody().front();
+    // rewriter.setInsertionPointToStart(&body3);
+    // Operation *clonedDequant = rewriter.clone(genOpDequant);
+    // // replacedOutputValue = genOpDequant.getResult(0);
+    // // yieldedResult = clonedTarget->getResult(0);
+
+    // rewriter.eraseOp(genOpMatmul);
+    // rewriter.eraseOp(genOpDequant);
+
+
+
+    // SmallVector<Value> workload;
+    // auto regionOp = rewriter.create<Flow::DispatchRegionOp>(
+    //   genOpMatmul.getLoc(), /*resultTypes=*/TypeRange(), /*dynamicDims=*/ValueRange(), workload);
+    // Flow::DispatchRegionOp regionOp;
+    // auto maybeRegionOp = Flow::wrapOpInDispatchRegion(rewriter, genOpMatmul);
+    // if (failed(maybeRegionOp))
+    //   return failure();
+    // regionOp = *maybeRegionOp;
+
+    // auto combinedRegionOp = Flow::movePrecedingOpsIntoDispatchRegion(rewriter, mmLhsDefiningOp, regionOp);
+    // if (failed(combinedRegionOp))
+    //   return failure();
+
+    // Block &body = combinedRegionOp->getBody().front();
+    // auto terminator = body.getTerminator();
+    // auto terminatorLoc = terminator->getLoc();
+    // rewriter.setInsertionPointAfter(combinedRegionOp);
+    // rewriter.create<Flow::ReturnOp>();
+    // std::cout << "mm parent: " << clonedMatmul->getParentOp() << std::endl;
+    // std::cout << "dq parent: " << clonedDequant->getParentOp() << std::endl;
+    // std::cout << "mm isAncestor: " << regionOp->isAncestor(clonedMatmul) << std::endl;
+    // std::cout << "dq isAncestor: " << regionOp->isAncestor(clonedDequant) << std::endl;
     return success();
 
   }
@@ -113,7 +247,7 @@ struct DequantizationMatmulFusePass
     : public DequantizationMatmulFuseBase<DequantizationMatmulFusePass> {
 
   void getDependentDialects(DialectRegistry &registry) const override {
-    registry.insert<linalg::LinalgDialect>();
+    registry.insert<linalg::LinalgDialect, Flow::FlowDialect>();
   }
 
   void runOnOperation() override {
