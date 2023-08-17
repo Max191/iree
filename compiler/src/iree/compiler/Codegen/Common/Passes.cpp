6976b372a5a4c517f9cfe981c4cce8606c72b4d6
Max Dawkins <max.dawkins@gmail.com>
2023-08-17 19:22:17 -0400

    Add transform interpreter preprocessing pass in codegen

--- compiler/src/iree/compiler/Codegen/Common/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/Common/Passes.cpp
@@ -11,12 +11,33 @@
 namespace mlir {
 namespace iree_compiler {
 
+llvm::cl::opt<std::string> clCodegenPreprocessingTransformDialectFileName(
+    "iree-codegen-use-preprocessing-transform-dialect-pass",
+    llvm::cl::desc(
+        "MLIR file containing a transform dialect specification to apply"),
+    llvm::cl::init(""));
+llvm::cl::opt<std::string> clCodegenPreprocessingTransformDialectDebugPayloadTag(
+    "iree-codegen-preprocessing-transform-dialect-debug-payload-tag",
+    llvm::cl::desc("tag attribute value for the transform dialect interpreter "
+                   "payload root operation"),
+    llvm::cl::init(""));
+llvm::cl::opt<std::string> clCodegenPreprocessingTransformDialectDebugTransformTag(
+    "iree-codegen-preprocessing-transform-dialect-debug-transform-tag",
+    llvm::cl::desc(
+        "tag attribute value for the transform dialect transform op container"),
+    llvm::cl::init(""));
+
 void addCommonTargetExecutablePreprocessingPasses(OpPassManager &passManager) {
   passManager.addNestedPass<func::FuncOp>(createTypePropagationPass());
   passManager.addPass(createBubbleUpOrdinalOpsPass());
   passManager.addPass(createBufferizeCopyOnlyDispatchesPass());
   passManager.addNestedPass<func::FuncOp>(
       IREE::LinalgExt::createDecomposeSoftmaxPass());
+  if (!clCodegenPreprocessingTransformDialectFileName.empty())
+      passManager.addPass(createTransformDialectInterpreterPass(
+          clCodegenPreprocessingTransformDialectFileName,
+          clCodegenPreprocessingTransformDialectDebugPayloadTag,
+          clCodegenPreprocessingTransformDialectDebugTransformTag));
 }
 
 //===---------------------------------------------------------------------===//
