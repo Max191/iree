bf730acdff6e8b7e7757db3acf9b84c48bf877f8
Max Dawkins <max.dawkins@gmail.com>
2024-02-29 17:15:25 -0500

    remove concat changes

--- compiler/src/iree/compiler/GlobalOptimization/DecomposeConcat.cpp
+++ compiler/src/iree/compiler/GlobalOptimization/DecomposeConcat.cpp
@@ -78,108 +78,6 @@ struct TransposeInnerConcatenation : public OpRewritePattern<tensor::ConcatOp> {
   }
 };
 
-static LogicalResult insertFoldingPrecondition(tensor::InsertSliceOp insertOp) {
-  auto definingOp = insertOp.getDest().getDefiningOp();
-  if (isa<tensor::ExpandShapeOp, tensor::EmptyOp>(definingOp)) {
-    return success();
-  }
-  return failure();
-}
-
-static void getNewInsertOperandMetadata(
-    tensor::InsertSliceOp insertOp,
-    std::optional<SmallVector<ReassociationIndices>> &collapsedSrcRe,
-    std::optional<SmallVector<ReassociationIndices>> &collapsedDstRe,
-    SmallVector<OpFoldResult> &collapsedOffsets,
-    SmallVector<OpFoldResult> &collapsedSizes,
-    SmallVector<OpFoldResult> &collapsedStrides) {
-  auto dstShape = insertOp.getDestType().getShape();
-  auto srcShape = insertOp.getSourceType().getShape();
-  auto mixedOffsets = insertOp.getMixedOffsets();
-  auto mixedSizes = insertOp.getMixedSizes();
-  auto mixedStrides = insertOp.getMixedStrides();
-  SmallVector<int64_t> collapsedDstShape, collapsedSrcShape;
-  SmallVector<int> srcNonUnitDims, dstNonUnitDims;
-  int rankDiff = dstShape.size() - srcShape.size();
-  for (auto [dstIdx, dstSize] : enumerate(dstShape)) {
-    if (dstSize != 1) {
-      collapsedDstShape.push_back(dstSize);
-      dstNonUnitDims.push_back(dstIdx);
-      int srcIdx = dstIdx - rankDiff;
-      if (srcIdx >= 0) {
-        collapsedSrcShape.push_back(srcShape[srcIdx]);
-        srcNonUnitDims.push_back(srcIdx);
-      }
-      collapsedOffsets.push_back(mixedOffsets[dstIdx]);
-      collapsedSizes.push_back(mixedSizes[dstIdx]);
-      collapsedStrides.push_back(mixedStrides[dstIdx]);
-    }
-  }
-  auto getReassociationIndices = [](SmallVector<int> nonUnitDims,
-                                    ArrayRef<int64_t> shape)
-      -> std::optional<SmallVector<ReassociationIndices>> {
-    SetVector<int> s(nonUnitDims.begin(), nonUnitDims.end());
-    SmallVector<ReassociationIndices> reInd(nonUnitDims.size(),
-                                            ReassociationIndices{});
-    int reListIdx = 0;
-    for (int idx = 0; idx < shape.size(); idx++) {
-      reInd[reListIdx].push_back(idx);
-      if (s.contains(idx)) {
-        reListIdx++;
-      }
-    }
-    if (llvm::all_of(reInd,
-                     [](ReassociationIndices re) { return re.size() == 1; })) {
-      return std::nullopt;
-    }
-    return reInd;
-  };
-  collapsedSrcRe = getReassociationIndices(srcNonUnitDims, srcShape);
-  collapsedDstRe = getReassociationIndices(dstNonUnitDims, dstShape);
-}
-
-// Concatenations with unit dims will decompose into insert slices with
-// Unit dims. This pattern rewrites the insert slices without unit dims
-// so unit dim folding is not blocked by these ops later on.
-struct FoldInsertSliceUnitDims
-    : public OpRewritePattern<tensor::InsertSliceOp> {
-  using OpRewritePattern::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(tensor::InsertSliceOp insertOp,
-                                PatternRewriter &rewriter) const override {
-    if (failed(insertFoldingPrecondition(insertOp))) {
-      return failure();
-    }
-
-    Location loc = insertOp.getLoc();
-    Value source = insertOp.getSource();
-    Value dest = insertOp.getDest();
-    std::optional<SmallVector<ReassociationIndices>> collapsedSrcRe,
-        collapsedDstRe;
-    SmallVector<OpFoldResult> collapsedOffsets, collapsedSizes,
-        collapsedStrides;
-    getNewInsertOperandMetadata(insertOp, collapsedSrcRe, collapsedDstRe,
-                                collapsedOffsets, collapsedSizes,
-                                collapsedStrides);
-    if (!collapsedDstRe) {
-      return success();
-    }
-    if (collapsedSrcRe) {
-      source = rewriter.create<tensor::CollapseShapeOp>(loc, source,
-                                                        collapsedSrcRe.value());
-    }
-    if (collapsedDstRe) {
-      dest = rewriter.create<tensor::CollapseShapeOp>(loc, dest,
-                                                      collapsedDstRe.value());
-    }
-    Value newInsert = rewriter.create<tensor::InsertSliceOp>(
-        loc, source, dest, collapsedOffsets, collapsedSizes, collapsedStrides);
-    rewriter.replaceOpWithNewOp<tensor::ExpandShapeOp>(
-        insertOp, insertOp.getDestType(), newInsert, collapsedDstRe.value());
-    return success();
-  }
-};
-
 struct DecomposeConcatPass : public DecomposeConcatBase<DecomposeConcatPass> {
   void getDependentDialects(DialectRegistry &registry) const override {
     registry.insert<linalg::LinalgDialect>();
@@ -198,7 +96,6 @@ struct DecomposeConcatPass : public DecomposeConcatBase<DecomposeConcatPass> {
       patterns.insert<TransposeInnerConcatenation>(context, /*benefit=*/2);
     }
     tensor::populateDecomposeTensorConcatPatterns(patterns);
-    // patterns.insert<FoldInsertSliceUnitDims>(context);
     if (failed(applyPatternsAndFoldGreedily(getOperation(),
                                             std::move(patterns)))) {
       return signalPassFailure();
