d1b96f0a950a47e9300a3e8cd9b81c815e819252
Max Dawkins <max.dawkins@gmail.com>
2025-04-16 13:26:59 -0500

    move bindings bufferization to after blockDynamicDimensions
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
+++ compiler/src/iree/compiler/Codegen/LLVMGPU/Passes.cpp
@@ -1172,10 +1172,10 @@ static void buildLLVMGPUCodegenConfigurationPassPipelineImpl(
     funcPassManager.addPass(createGPUGeneralizeNamedOpsPass);
     funcPassManager.addPass(createROCDLConfigureBufferInstructionsPass);
     addCommonTargetExecutablePreprocessingPasses(funcPassManager);
-    funcPassManager.addPass(createBufferizeDispatchTensorLoadStorePass);
     // This materializes into 'nop' in the absence of pad encoding layout
     // attributes.
     funcPassManager.addPass(createBlockDynamicDimensionsPass);
+    funcPassManager.addPass(createBufferizeDispatchTensorLoadStorePass);
     funcPassManager.addPass(createConfigTrackingCanonicalizerPass);
     funcPassManager.addPass(createCSEPass);
   }
